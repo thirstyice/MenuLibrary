0e44f60521ba3d2581bcf9eaa69aa53fbcbdadf8
thirstyice <tauranwood@gmail.com>
2024-05-12 15:48:11 -0600

    Add support for IPAddress

--- src/MenuIP.cpp
+++ src/MenuIP.cpp
@@ -9,3 +9,7 @@ MenuIP::MenuIP(String _title, uint8_t* firstOctet, uint8_t* secondOctet, uint8_t
 		new MenuValues<uint8_t>(fourthOctet, 255, 0, 1)
 	);
 }
+
+MenuIP::MenuIP(String _title, IPAddress* ip) {
+	MenuIP(_title, &ip->operator[](0), &ip->operator[](1), &ip->operator[](2), &ip->operator[](3));
+}
--- src/MenuIP.h
+++ src/MenuIP.h
@@ -2,7 +2,10 @@
 
 #include "MenuValue.h"
 
+#include <IPAddress.h>
+
 class MenuIP final : public MenuValue {
 public:
 	MenuIP(String _title, uint8_t* firstOctet, uint8_t* secondOctet, uint8_t* thirdOctet, uint8_t* fourthOctet);
+	MenuIP(String _title, IPAddress* ip);
 };
