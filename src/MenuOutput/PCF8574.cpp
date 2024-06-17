70651e94e7727f50e4e79f6d0e6b5d4813dc954e
thirstyice <thirstyice@users.noreply.github.com>
2024-06-17 03:15:05 -0600

    Update PCF8574 to new drawing strategy

--- src/MenuOutput/PCF8574.cpp
+++ src/MenuOutput/PCF8574.cpp
@@ -1,50 +1,58 @@
 #include "PCF8574.h"
 
 
-void MenuOutputPCF8574::doOutput(uint8_t startIndex, uint8_t numLines) {
-	lcd->clear();
-	lcd->home();
-	uint8_t startOfLine = startIndex;
-	for (uint8_t i=0; i<numLines; i++) {
-		uint8_t endOfLine = currentContents.indexOf('\n', startOfLine);
-		String line = currentContents.substring(startOfLine, endOfLine);
-		lcd->setCursor(0,i);
-		if (line.charAt(0) != MenuChar[MenuChars::StartOfSelection]) {
-			line = " " + line;
+
+void MenuOutputPCF8574::drawLine(uint8_t lineIndex, String line) {
+	line.replace(MenuChar[MenuChars::BackArrow], (char)127);
+	int characterIndex = line.indexOf(MenuChar[MenuChars::StartOfSelection]);
+	while (characterIndex != -1) {
+		line[characterIndex] = textCursor;
+		if (characterIndex!=0 && line.charAt(characterIndex-1)==MenuChar[MenuChars::ReplaceableWithCursor]) {
+			line.remove(characterIndex-2,2);
 		}
-		line.replace(MenuChar[MenuChars::BackArrow], (char)127);
-		int characterIndex = line.indexOf(MenuChar[MenuChars::StartOfSelection]);
+		characterIndex = line.indexOf(MenuChar[MenuChars::StartOfSelection]);
+	}
+	uint8_t alignRightFrom = line.indexOf(MenuChar[MenuChars::AlignRightFollowing]);
+	line.replace(MenuChars::AlignRightFollowing, ' ');
+	for (uint8_t character=0; character<MenuChars::Count; character++) {
+		characterIndex = line.indexOf(MenuChar[character]);
 		while (characterIndex != -1) {
-			line[characterIndex] = textCursor;
-			if (characterIndex!=0 && line.charAt(characterIndex-1)==MenuChar[MenuChars::ReplaceableWithCursor]) {
-				line.remove(characterIndex-2,2);
-			}
-			characterIndex = line.indexOf(MenuChar[MenuChars::StartOfSelection]);
-		}
-		for (uint8_t character=0; character<MenuChars::Count; character++) {
-			if (character == MenuChars::AlignRightFollowing) {
+			if (characterIndex == (int)line.length()) {
+				// That's the null at the end of the string, don't delete that!
 				break;
 			}
+			line.remove(characterIndex, 1);
 			characterIndex = line.indexOf(MenuChar[character]);
-			while (characterIndex != -1) {
-				if (characterIndex == (int)line.length()) {
-					break;
-				}
-				line.remove(characterIndex, 1);
-				characterIndex = line.indexOf(MenuChar[character]);
-			}
 		}
-		if (line.indexOf(MenuChar[MenuChars::AlignRightFollowing]) != -1) {
-			lcd->print(line.substring(0,line.indexOf(MenuChar[MenuChars::AlignRightFollowing])));
-			lcd->rightToLeft();
-			lcd->setCursor(width-1,i);
-			for (uint8_t j=(line.length()-1); j>line.lastIndexOf(MenuChar[MenuChars::AlignRightFollowing]); j--) {
-				lcd->print(line[j]);
-			}
-			lcd->leftToRight();
-		} else {
-			lcd->print(line);
-		}
-		startOfLine = endOfLine+1;
 	}
+	lcd->setCursor(0, lineIndex);
+	lcd->print((lineIndex==focusedLine)?(char)textCursor:' ');
+	if (width > line.length() && alignRightFrom != -1) {
+		lcd->print(line.substring(0, alignRightFrom));
+		for (uint8_t i=line.length(); i<width; i++) {
+			lcd->print(' ');
+		}
+		lcd->rightToLeft();
+		lcd->setCursor(width-1, lineIndex);
+		for (uint8_t j=(line.length()-1); j>alignRightFrom; j--) {
+			lcd->print(line[j]);
+		}
+		lcd->leftToRight();
+	} else {
+		lcd->print(line);
+		for (uint8_t i=line.length(); i<width; i++) {
+			lcd->print(' ');
+		}
+	}
+}
+
+void MenuOutputPCF8574::setFocusedLine(uint8_t line) {
+	if (line==focusedLine) {
+		return;
+	}
+	lcd->setCursor(0, focusedLine);
+	lcd->print(' ');
+	lcd->setCursor(0, line);
+	lcd->print((char)textCursor);
+	focusedLine = line;
 }
--- src/MenuOutput/PCF8574.h
+++ src/MenuOutput/PCF8574.h
@@ -10,9 +10,10 @@ public:
 	MenuOutputPCF8574(LiquidCrystal_PCF8574* _lcd, uint8_t _width, uint8_t _height) : 
 		lcd(_lcd) {width = _width; height = _height;}
 	void setCursor(unsigned char newCursor) {textCursor = newCursor;};
+	void drawLine(uint8_t line, String contents);
+	void setFocusedLine(uint8_t line);
 
 private:
-	void doOutput(uint8_t startIndex, uint8_t numLines);
 	unsigned char textCursor = 126;
 	LiquidCrystal_PCF8574* lcd;
 };
