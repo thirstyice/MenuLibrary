7c4770ccab1545871aa428633419c01eccf4c63e
thirstyice <thirstyice@users.noreply.github.com>
2024-09-07 15:36:08 -0600

    Add handling of title breaks
    MenuValue still has issues

--- src/MenuOutput/GenericTextBased.cpp
+++ src/MenuOutput/GenericTextBased.cpp
@@ -28,5 +28,68 @@ void MenuOutputGenericTextBased::drawLine(uint8_t line, String contents) {
 			contents.remove(i,1);
 		}
 	}
+	trimForOutput(&contents);
 	outputLine(line, &contents);
+}
+
+void MenuOutputGenericTextBased::removeBreakChars(String* out) {
+	for (
+		int index = out->indexOf(getControlChar(MenuChars::Break));
+		index>-1;
+		index = out->indexOf(getControlChar(MenuChars::Break)))
+	{
+		out->remove(index, 1);
+	}
+}
+uint8_t MenuOutputGenericTextBased::countBreakChars(String* out) {
+	uint8_t breakChars = 0;
+	for (
+		int index = out->indexOf(getControlChar(MenuChars::Break));
+		index>-1;
+		index = out->indexOf(getControlChar(MenuChars::Break), index+1))
+	{
+		breakChars ++;
+	}
+	return breakChars;
+}
+void MenuOutputGenericTextBased::trimForOutput(String* out) {
+	uint8_t breakChars = countBreakChars(out);
+	if (out->length()-breakChars <= width) {
+		removeBreakChars(out);
+		return;
+	}
+	int selection = out->indexOf(getControlChar(MenuChars::StartOfSelection));
+	if (selection<=0) {
+		removeBreakChars(out);
+		out->remove(width);
+		return;
+	}
+	if (breakChars == 0) {
+		*out = out->substring(selection-1, (selection + width)-1);
+		return;
+	}
+	// Start by removing the title
+	*out = out->substring(out->indexOf(getControlChar(MenuChars::Break))+1);
+	selection = out->indexOf(getControlChar(MenuChars::StartOfSelection));
+	breakChars--;
+	if ((out->length()-breakChars)<=width) {
+		removeBreakChars(out);
+		return;
+	}
+	// Trim the start to the break before the selection
+	int preBreak = out->lastIndexOf(getControlChar(MenuChars::Break), selection);
+	if (preBreak>=0) {
+		*out = out->substring(preBreak);
+	}
+	removeBreakChars(out);
+	selection = out->indexOf(getControlChar(MenuChars::StartOfSelection));
+	if (selection>=width-1) {
+		if (selection+width>=out->length()) {
+			*out = out->substring((out->length()-width)+1);
+			return;
+		}
+		*out = out->substring(selection-1);
+	}
+	out->remove(width);
+	return;
 }
