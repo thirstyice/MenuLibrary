ccd4e6d4242f1abae0f02458f4b9e3cc4cf2ebc8
thirstyice <tauranwood@gmail.com>
2026-03-23 14:22:17 -0600

    MenuString: Use pass by reference

--- src/MenuString.cpp
+++ src/MenuString.cpp
@@ -1,14 +1,14 @@
 #include "MenuString.h"
 
-MenuString* MenuString::setString(String* _string) {
+MenuString* MenuString::setString(String& _string) {
 	string = _string;
 	return this;
 }
 
 MenuString* MenuString::setMaxLength(uint8_t _maxLength) {
 	maxLength = _maxLength;
-	if (string->length()>maxLength) {
-		string->remove(maxLength);
+	if (string.length()>maxLength) {
+		string.remove(maxLength);
 	}
 	return this;
 }
@@ -17,20 +17,20 @@ String MenuString::getTitle() {
 	hasChanges = false;
 	String outputString = title + MenuChar[MenuChars::Break] + MenuChar[MenuChars::AlignRightFollowing];
 	if (isOpen) {
-		outputString += string->substring(0, index);
+		outputString += string.substring(0, index);
 		outputString += MenuChar[MenuChars::StartOfSelection];
-		outputString += string->charAt(index);
+		outputString += string.charAt(index);
 		outputString += MenuChar[MenuChars::EndOfSelection];
-		outputString += string->substring(index+1);
+		outputString += string.substring(index+1);
 	} else {
-		outputString += *string;
+		outputString += string;
 	}
 	return outputString;
 }
 
 bool MenuString::needsRedraw() {
-	if (*string != lastValue) {
-		lastValue = *string;
+	if (string != lastValue) {
+		lastValue = string;
 		hasChanges = true;
 	}
 	return hasChanges;
@@ -40,8 +40,8 @@ MenuReaction MenuString::engage() {
 	hasChanges = true;
 	if (isOpen) {
 		index++;
-		if (index>string->length() || (maxLength?index>maxLength:false)) {
-			string->trim();
+		if (index>string.length() || (maxLength?index>maxLength:false)) {
+			string.trim();
 			isOpen = false;
 			index = 0;
 			return MenuReaction::closeDown;
@@ -59,7 +59,7 @@ MenuReaction MenuString::disengage() {
 	if (isOpen) {
 		if (index==0) {
 			isOpen = false;
-			string->trim();
+			string.trim();
 			return MenuReaction::closeDown;
 		}
 		index--;
@@ -68,25 +68,25 @@ MenuReaction MenuString::disengage() {
 }
 
 MenuReaction MenuString::increase() {
-	if (index==string->length()) {
-		*string = *string + ' ';
+	if (index==string.length()) {
+		string = string + ' ';
 	}
-	char newChar = string->charAt(index) + 1;
+	char newChar = string.charAt(index) + 1;
 	if (newChar>126) {
 		newChar = 32;
 	}
-	string->setCharAt(index, newChar);
+	string.setCharAt(index, newChar);
 	return MenuReaction::changeValue;
 }
 
 MenuReaction MenuString::decrease() {
-	if (index==string->length()) {
-		*string = *string + ' ';
+	if (index==string.length()) {
+		string = string + ' ';
 	}
-	char newChar = string->charAt(index) - 1;
+	char newChar = string.charAt(index) - 1;
 	if (newChar<32) {
 		newChar = 126;
 	}
-	string->setCharAt(index, newChar);
+	string.setCharAt(index, newChar);
 	return MenuReaction::changeValue;
 }
--- src/MenuString.h
+++ src/MenuString.h
@@ -4,13 +4,13 @@
 
 class MenuString: public MenuBase<MenuString> {
 public:
-	MenuString(String _title, String* str) : MenuBase(_title), string(str) {}
-	MenuString* setString(String* str);
+	MenuString(String _title, String& str) : MenuBase(_title), string(str) {}
+	MenuString* setString(String& str);
 	MenuString* setMaxLength(uint8_t len);
 	String getTitle() override;
 	bool needsRedraw() override;
 private:
-	String* string = nullptr;
+	String& string;
 	uint8_t maxLength=0;
 	String lastValue;
 	uint8_t index = 0;
