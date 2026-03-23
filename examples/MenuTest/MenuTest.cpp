98e57651a8369e4451d342c9a287ddf372044dd7
thirstyice <tauranwood@gmail.com>
2026-03-23 14:52:27 -0600

    Update example for pass by reference

--- examples/MenuTest/MenuTest.cpp
+++ examples/MenuTest/MenuTest.cpp
@@ -81,9 +81,9 @@ Menu menu{
 		new MenuValues<float>(&valueTestFloat, 1, 0, 0.1)
 	),
 	new MenuIP("IP(8x4):", &ipTest1[0], &ipTest1[1], &ipTest1[2], &ipTest1[3]),
-	new MenuIP("IP(32):", &ipTest2),
-	new MenuString("String:", &stringTest),
-	new MenuString("Str:", &shortStr)
+	new MenuIP("IP(32):", ipTest2),
+	new MenuString("String:", stringTest),
+	new MenuString("Str:", shortStr)
 };
 
 
