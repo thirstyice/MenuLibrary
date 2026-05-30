ecbabe4584b28662b30a6ac15b7e288e426995f9
thirstyice <thirstyice@users.noreply.github.com>
2026-05-30 14:52:20 -0600

    Add docs for MenuString

--- src/MenuString.h
+++ src/MenuString.h
@@ -2,10 +2,31 @@
 
 #include "MenuBase.h"
 
+/**
+** @brief Defines a Menu item that can edit a String
+**
+**/
 class MenuString: public MenuBase<MenuString> {
 public:
+	/**
+	 * @brief Create a MenuString
+	 * @param _title The title to show before the editable string
+	 * @param str The editable string
+	 */
 	MenuString(const char* _title, String& str) : MenuBase(_title), string(str) {}
+	/**
+	** @brief Change the editable String
+	**
+	** @param str The new String variable
+	** @return ** MenuString& Allows method chaining
+	**/
 	MenuString& setString(String& str);
+	/**
+	** @brief Set the maximum length of the editable String
+	**
+	** @param len The new max length
+	** @return ** MenuString& Allows method chaining
+	**/
 	MenuString& setMaxLength(uint8_t len);
 	TitleFlags getTitle(char* buf, const uint8_t& len) override;
 	bool needsRedraw() override;
